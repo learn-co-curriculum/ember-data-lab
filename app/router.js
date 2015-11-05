484d9238b1dff1773228f8a4bb4aefe66d0de303
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2015-11-05 15:52:54 -0500

    created landing page

--- app/router.js
+++ app/router.js
@@ -5,7 +5,7 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-export default Router.map(function() {  
+export default Router.map(function() { 
   this.route('resources', function() {
     this.route('resource', { path: '/:resource_id' });
   });
